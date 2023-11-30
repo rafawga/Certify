3a8421df1a9598762a5c6adb7a33559c3819bdad
Rafael Guiterrez <111995483+rafawga@users.noreply.github.com>
2023-11-29 22:38:58 -0300

    Update Model.dart

--- Pages/CursoDetail/Model.dart
+++ Pages/CursoDetail/Model.dart
@@ -11,7 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-
+ 
 class CursoDetailModel extends FlutterFlowModel<CursoDetailWidget> {
   ///  State fields for stateful widgets in this page.
 
